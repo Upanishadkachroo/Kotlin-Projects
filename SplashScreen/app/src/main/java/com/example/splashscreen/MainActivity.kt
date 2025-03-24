dd9ca70a033e762b4f3fa27cae771475494b9b84
Upanishadkachroo <upanishadkachroo@gmail.com>
2025-03-25 00:51:42 +0530

    working on sign up

--- SplashScreen/app/src/main/java/com/example/splashscreen/MainActivity.kt
+++ SplashScreen/app/src/main/java/com/example/splashscreen/MainActivity.kt
@@ -2,11 +2,7 @@ package com.example.splashscreen
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import java.util.logging.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +10,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         android.os.Handler().postDelayed({
-            val intent=Intent(this, MainActivity2::class.java)
+            val intent=Intent(this, SignUp::class.java)
             startActivity(intent)
 //            finish() //to get back to the previou screen
         }, 3000)

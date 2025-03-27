58caeb370cd3ff9c88bd0ea0bd19e74120467659
Upanishadkachroo <upanishadkachroo@gmail.com>
2025-03-28 00:10:54 +0530

    sign-up page completed

--- SplashScreen/app/src/main/java/com/example/splashscreen/SignUp.kt
+++ SplashScreen/app/src/main/java/com/example/splashscreen/SignUp.kt
@@ -1,14 +1,12 @@
 package com.example.splashscreen
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
 class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.Signup)
+        setContentView(R.layout.activity_second)
 
 
     }

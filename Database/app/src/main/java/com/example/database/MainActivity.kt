efb992a673636e155c31d948af0f0eae007f4417
Upanishadkachroo <upanishadkachroo@gmail.com>
2025-04-20 12:23:32 +0530

    firebase connected

--- Database/app/src/main/java/com/example/database/MainActivity.kt
+++ Database/app/src/main/java/com/example/database/MainActivity.kt
@@ -1,16 +1,56 @@
-package com.example.database
+package com.example.day16database
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import User
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.example.database.R
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlin.math.sign
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var database : DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val signButton = findViewById<Button>(R.id.btnSignUp)
+        val etName = findViewById<TextInputEditText>(R.id.etName)
+        val etMail = findViewById<TextInputEditText>(R.id.etMail)
+        val userId = findViewById<TextInputEditText>(R.id.etUserName)
+        val userPassword = findViewById<TextInputEditText>(R.id.etPassword)
+
+        signButton.setOnClickListener {
+
+            val name = etName.text.toString()
+            val mail = etMail.text.toString()
+            val uniqueId = userId.text.toString()
+            val password = userPassword.text.toString()
+
+            val user = User(name, mail, password, uniqueId)
+            database = FirebaseDatabase.getInstance().getReference("Users")
+
+            database.child(uniqueId).setValue(user).addOnSuccessListener {
+                etName.text?.clear()
+                Toast.makeText(this, "User Registered", Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener {
+                Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+//        val signIntext = findViewById<TextView>(R.id.tvSignIN)
+//        signIntext.setOnClickListener {
+//            val openSignInActivity = Intent(this, SignInActivity::class.java)
+//            startActivity(openSignInActivity)
+//        }
     }
 }
